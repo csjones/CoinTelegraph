b3ee8a224a4d3e5b8fae7d15363cdc17edaeafba
Chris <crookiemoonster+github@gmail.com>
2014-07-04 23:34:50 -0400

    Added initial code

---
+++ CoinTelegraph/CoinTelegraph/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoinTelegraph
+//
+//  Created by Chris on 7/4/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoinTelegraph/CoinTelegraph/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoinTelegraph
+//
+//  Created by Chris on 7/4/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
