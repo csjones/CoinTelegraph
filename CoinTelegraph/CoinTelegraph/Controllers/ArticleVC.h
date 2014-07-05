79abd5b0f4ef7771cda502553edd0c596321c513
Chris <crookiemoonster+github@gmail.com>
2014-07-05 17:15:16 -0400

    Added ArticleVC

---
+++ CoinTelegraph/CoinTelegraph/Controllers/ArticleVC.h
@@ -0,0 +1,13 @@
+//
+//  ArticleVC.h
+//  CoinTelegraph
+//
+//  Created by Chris on 7/5/14.
+//  Copyright (c) 2014 GigaBitcoin, LLC. All rights reserved.
+//
+
+@interface ArticleVC : UIViewController
+
+@property ( weak, nonatomic )   IBOutlet    UIWebView* webView;
+
+@end
